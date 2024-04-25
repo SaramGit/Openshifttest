2b889109a8a74ab971d257d8ce97a5010b6ae088
SaramGit <khansaram422@gmail.com>
2024-04-25 15:48:34 +0530

    appresource_Check

--- src/main/java/com/nt/DummyKubernativeAppApplication.java
+++ src/main/java/com/nt/DummyKubernativeAppApplication.java
@@ -1,5 +1,6 @@
 package com.nt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-//@EnableAutoConfiguration()
-//@ComponentScan
-
 public class DummyKubernativeAppApplication {
+
+	@Value("${my.name}")
+	private String name;
 	
 	@GetMapping("/")
 	public String getResponse() {
-		return "Hello bandhu";
+		return "Hello bandhu "+name;
 	}
    
 	public static void main(String[] args) {
--- src/test/java/com/nt/DummyKubernativeAppApplicationTests.java
+++ src/test/java/com/nt/DummyKubernativeAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.nt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class DummyKubernativeAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void checkEvenOrNot() {
+		int num=4;
+		assertEquals(2, 4/2);
 	}
 
 }
